afa4fc21f26578b410ceaae9be27b4cb8137a26d
NextFailed <lars.huebner@proton.me>
2026-06-29 22:39:49 +0200

    Erstelle Methode zum Einlesen von Objekten in der szene.txt

--- src/IO/FileHandler.java
+++ src/IO/FileHandler.java
@@ -5,9 +5,10 @@ import de.hsrm.mi.prog2.TextIO;
 
 import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.Constructor;
 import java.util.ArrayList;
 
-public class AkteurCreator {
+public class FileHandler {
 
     public static ArrayList<Akteur> readAkteureAsList(File file) {
         ArrayList<Akteur> akteure = new ArrayList<>();
@@ -15,7 +16,7 @@ public class AkteurCreator {
             ArrayList<String> fileStream = TextIO.read(file);
 
             for(String currentLine : fileStream) {
-                String[] arguments = splitAkteurArguments(currentLine);
+                String[] arguments = splitArguments(currentLine);
                 Akteur newAkteur = createAkteur(arguments);
                 akteure.add(newAkteur);
             }
@@ -32,7 +33,27 @@ public class AkteurCreator {
         return akteure;
     }
 
-    private static String[] splitAkteurArguments(String line) throws IllegalArgumentException {
+    public static ArrayList<Object> readSceneAsList(File file) {
+        ArrayList<Object> szene = new ArrayList<>();
+        try {
+            ArrayList<String> fileStream = TextIO.read(file);
+
+            for(String currentLine : fileStream) {
+                String[] arguments = splitArguments(currentLine);
+                Object leckerbissen = createLeckerbissen(arguments);
+                szene.add(leckerbissen);
+            }
+        }
+        catch(IOException e) {
+            e.printStackTrace();
+        }
+        catch(NullPointerException e) {
+            System.err.printf(e.getMessage());
+        }
+        return szene;
+    }
+
+    private static String[] splitArguments(String line) throws IllegalArgumentException {
         if(!line.contains(",")) {
             throw new IllegalArgumentException("Argumente wurden nicht mit ',' getrennt.");
         }
@@ -40,13 +61,12 @@ public class AkteurCreator {
     }
 
     private static Akteur createAkteur(String[] arguments) throws ClassNotFoundException {
+        String akteurKlasse = arguments[0];
         String akteurName = arguments[1];
         Nahrungstyp akteurNahrungstyp = Nahrungstyp.valueOf(arguments[2]);
         Esstyp akteurEsstyp = Esstyp.valueOf(arguments[3]);
         int akteurGewicht = Integer.parseInt(arguments[4]);
         int akteurAppetitGrenze = Integer.parseInt(arguments[5]);
-
-        String akteurKlasse = arguments[0];
 
         if(akteurKlasse.equalsIgnoreCase("fisch")) {
             return new Fisch(akteurName, akteurGewicht, akteurNahrungstyp, akteurEsstyp, akteurAppetitGrenze);
@@ -67,4 +87,24 @@ public class AkteurCreator {
         return null;
     }
 
+    private static Object createLeckerbissen(String[] arguments) {
+        try {
+            String leckerbissenKlasse = arguments[0];
+            int leckerbissenGewicht = Integer.parseInt(arguments[1]);
+            Class<?> klasse = Class.forName(arguments[0]);
+            Constructor<?> konstruktor = klasse.getDeclaredConstructor(int.class);
+            Object leckerbissen = konstruktor.newInstance(leckerbissenGewicht);
+            return leckerbissen;
+        }
+        catch(ClassNotFoundException e) {
+            System.err.println("Konnte keine Klasse mit dem Namen '" + arguments[0] + "' finden.");
+        }
+        catch(NoSuchMethodException e) {
+            System.err.println("Die Klasse '" + arguments[0] + "' besitzt keinen passenden Konstruktor.");
+        }
+        catch(Exception e) {
+            System.err.println("Konnte keine Instanz der Klasse '" + arguments[0] + "' mit Parameter '" + arguments[1] + "' erstellen.");
+        }
+        return null;
+    }
 }
--- src/Meer.java
+++ src/Meer.java
@@ -1,16 +1,18 @@
 import java.io.File;
 import java.util.ArrayList;
 
-import IO.AkteurCreator;
+import IO.FileHandler;
 import actors.Akteur;
+import actors.Leckerbissen;
 
 public class Meer {
 
     public static void main(String[] args) {
 
         File akteureDatei = new File(args[0]);
-//        File szeneDatei = new File(args[1]);
+        File szeneDatei = new File(args[1]);
 
-        ArrayList<Akteur> akteure = AkteurCreator.readAkteureAsList(akteureDatei);
+        ArrayList<Akteur> akteure = FileHandler.readAkteureAsList(akteureDatei);
+        ArrayList<Leckerbissen> leckerbissens = FileHandler.readSceneAsList(szeneDatei);
     }
 }
